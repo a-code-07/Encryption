b9a8f3127291075608ddb96f943e8a896c8408c7
Abhishek Kumar Singh <abhisheksingh007@outlook.in>
2019-08-11 21:57:06 +0530

    Adding One String in Every Model And USing This

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Insert Data To Encrypt:");
+            var val = Console.ReadLine();
+            var enc = Encryption.Encrypt(val,true);
+            Console.WriteLine($"Encrypted: {enc}");
+          
+        }
+    }
+}
